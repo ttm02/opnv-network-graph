6a32bd16360d2a16c32139847ec1e42353e725ba
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-02-14 18:05:13 +0100

    added progres bar

--- build_full_network.py
+++ build_full_network.py
@@ -1,5 +1,6 @@
 import os
 from time import strftime
+from tqdm import tqdm
 
 from parse_input_file import get_line_info_from_file
 from network import Network
@@ -11,19 +12,27 @@ dir_to_read="data/20250210_fahrplaene_gesamtdeutschland"
 network = Network()
 all_stops = {}
 
-for root, dirs, files in os.walk(dir_to_read):
-    path = root.split(os.sep)
-    for file in files:
-        if file.endswith(".xml"):
-            qualified_path = os.path.join(root, file)
-            print("Read " + qualified_path)
-            new_network, stops = get_line_info_from_file(qualified_path)
-            network.merge(new_network)
-            for stop_id, info in stops.items():
-                if stop_id not in all_stops:
-                    all_stops[stop_id] = info
-                else:
-                    assert (all_stops[stop_id] == info)
+# Pre-count total files
+total_files = sum(len(files) for _, _, files in os.walk(dir_to_read))
+
+with tqdm(total=total_files, desc="Parse files", unit="files") as pbar:
+    for root, dirs, files in os.walk(dir_to_read):
+        path = root.split(os.sep)
+        for file in files:
+            if file.endswith(".xml"):
+                qualified_path = os.path.join(root, file)
+                #print("Read " + qualified_path)
+                new_network, stops = get_line_info_from_file(qualified_path)
+                network.merge(new_network)
+                for stop_id, info in stops.items():
+                    if stop_id not in all_stops:
+                        all_stops[stop_id] = info
+                    else:
+                        pass
+                        # there is some ambiguity in the data regarding stop naming
+                        #assert (all_stops[stop_id] == info)
+
+            pbar.update(1)
 
 print("stops:")
 print(len(all_stops))
