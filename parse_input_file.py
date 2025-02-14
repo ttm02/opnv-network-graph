cf0b5658bdcf1bd310897f7d3fd09fc593510cdc
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-02-14 15:04:04 +0100

    fix assertion error

--- parse_input_file.py
+++ parse_input_file.py
@@ -5,30 +5,28 @@ from lxml import etree
 
 file_to_read="data/20250210_fahrplaene_gesamtdeutschland/126_HEAGTRAM/NX-PI-01_DE_NAP_LINE_126-HEAGTRAM-6_20250208.xml"
 
-tree = etree.parse(file_to_read)
-root = tree.getroot()
-assert root.get("version") == "ntx:1.1"
-xml_namespace="http://www.netex.org.uk/netex"
+xml_namespace="{http://www.netex.org.uk/netex}"
+
 
 def get_single_children(root, child_type):
     chlds = [c for c in root.getchildren() if c.tag == xml_namespace+child_type]
-    print(chlds)
     assert len(chlds) == 1
     return chlds[0]
 
 
 
 def get_frame_type(frame):
-    return (frame,"TypeOfFrameRef").get("ref")
+    return get_single_children(frame,"TypeOfFrameRef").get("ref")
 
 line_data=dict()
 stops=dict()
-def get_line_info_from_file(data):
 
+def get_line_info_from_file(file_to_read):
     tree = etree.parse(file_to_read)
     root = tree.getroot()
     assert root.get("version") == "ntx:1.1"
     composite_frame = get_single_children(get_single_children(root,"dataObjects"),"CompositeFrame")
+
 
     # check that this is indeed a line definition
     assert get_frame_type(composite_frame)=="epip:EU_PI_LINE_OFFER"
